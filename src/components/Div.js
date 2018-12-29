f4e7b90012286198a3fdb04c1716119bc5fc496c
damianhouse <damianhouse@gmail.com>
2018-12-28 20:25:22 -0500

    Removed storybook

--- src/components/Div.js
+++ src/components/Div.js
@@ -1,11 +1,7 @@
 import React from "react";
 
 const Div = props => {
-    return (
-        <div {...props} style={styles}>
-            {props.children}
-        </div>
-    );
+    return <div {...props}>{props.children}</div>;
 };
 
 export default Div;
